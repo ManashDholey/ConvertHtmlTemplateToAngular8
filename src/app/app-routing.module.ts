8131caa98ae004ab4bd8a3214a3216c1cc745193
Manash Dholey <53951910+ManashDholey@users.noreply.github.com>
2024-07-26 11:17:48 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component:AppComponent
+  },
+{
+    path:'ConvertHtmlTemplateToAngular8',
+    component:AppComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
